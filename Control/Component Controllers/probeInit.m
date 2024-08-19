015ab75f5bf48aa08911cf6c4e70095e3e6554ec
jiversivers <jdivers@uark.edu>
2024-08-19 13:15:26 -0500

    moved to subdirs

--- Control/Component Controllers/probeInit.m
+++
@@ -1,6 +0,0 @@
-%% probeInit
-function probe = probeInit(port)
-probe = serialport(port, 115200, "Parity", "none", "DataBits", 8, "StopBits", 1, "FlowControl", "None");
-probe.setDTR(true)
-probe.setRTS(true)
-end
