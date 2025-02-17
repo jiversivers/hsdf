ee9aa95987a851638bc5bfe3e7ea19e9e0c33b49
jdivers <jdivers@10.1.89.130>
2025-02-17 12:36:20 -0600

    replaced version check conditional with try-catch

--- Control/Component Controllers/VS Custom Controllers/getWavelength.m
+++ Control/Component Controllers/VS Custom Controllers/getWavelength.m
@@ -2,32 +2,42 @@ function [wl, err] = getWavelength(device)
 
 y = version('-release');
 y = str2num(y(1:4));
-if isMATLABReleaseOlderThan("R2020b")
 
-    % Pre-COVID (lol) MATLAB
-    command = convertCharsToStrings('W  ?');
+try ~isMATLABReleaseOlderThan("R2020b");
+    isOld = false;
+catch
+    isOld = true;
+end
 
-    % Send serial command to query (W)avelength to input
-    fprintf(device, command);
-    fgets(device);
-    
-    % Check for answer
-    wl_chars = readline(device);
-    
-    if wl_chars == command
-        err = 1;
-        warning('LCTF did not return expected response.')
-    else
-        wl = str2num(wl_chars{1}(4:end));
-    end
-
-else
+if ~isOld %~isMATLABReleaseOlderThan("R2020b")
     % Post-COVID MATLAB
     command = convertCharsToStrings('W  ?');
     
     % Send serial command to query (W)avelength to input
     writeline(device, command);
     readline(device);
+    
+    % Check for answer
+    wl_chars = readline(device);
+    
+    if wl_chars == command
+        err = 1;
+        error('LCTF did not return expected response.')
+    else
+        wl = str2num(wl_chars{1}(4:end));
+        if isempty(wl)
+            err = 1;
+            error('LCTF did not return any response. Try setting the wavelength again and rechecking.')
+        end
+    end
+    
+else
+    % Pre-COVID (lol) MATLAB
+    command = convertCharsToStrings('W  ?');
+
+    % Send serial command to query (W)avelength to input
+    fprintf(device, command);
+    fgets(device);
     
     % Check for answer
     wl_chars = readline(device);
